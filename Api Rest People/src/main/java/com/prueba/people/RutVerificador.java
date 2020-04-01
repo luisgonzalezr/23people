89bc1e365ce0ff37318aabfc838f4974d3bede05
govindo <govindo@govindo>
2020-04-01 18:40:25 -0300

    Se modificó el verificador de Rut.. No estaba funcionando correctamente

--- Api Rest People/src/main/java/com/prueba/people/RutVerificador.java
+++ Api Rest People/src/main/java/com/prueba/people/RutVerificador.java
@@ -1,20 +1,11 @@
 package com.prueba.people;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class RutVerificador {
-
-	 public static boolean validar(String rut){
-		 			rut = rut.replace(".", "");
-			 Pattern pattern = Pattern.compile("^[0-9]+-[0-9kK]{1}$");
-			 Matcher matcher = pattern.matcher(rut);
-				if ( matcher.matches() == false ) return false;
-	    	
+	    static public boolean validar(String rut){
 	        int suma=0;
 	        String dvR,dvT;
 	        int[] serie = {2,3,4,5,6,7};
-	       
+	        rut = rut.replace(".", "");
 	        rut = rut.replace("-", "");
 	        dvR = rut.substring(rut.length()-1);
 	        for(int i = rut.length()-2;i>=0; i--){
@@ -25,16 +16,13 @@ public class RutVerificador {
 	        if(dvT.compareToIgnoreCase("10") == 0){
 	            dvT = "K";
 	        }
-
+	
 	        if(dvT.compareToIgnoreCase(dvR) == 0){
 	            return true;
 	        } else {
 	            return false;
 	        }
 	    }
-	
-	
+	}
 
-}
-	
-	
+
